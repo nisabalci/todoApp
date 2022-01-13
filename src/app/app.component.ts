6305c300851ec8c98fb240ba54b0df4a8523e747
nisa <nisabalci@smartiys.com.tr>
2022-01-13 17:30:45 +0300

    feat: add method added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Model } from './model';
+import { Model, TodoItem } from './model';
 
 @Component({
   selector: 'app-root',
@@ -15,5 +15,11 @@ export class AppComponent {
 
   getItems() {
     return this.model.items.filter(item => !item.action);
+  }
+
+  addItem(value: string) {
+    if (value != "") {
+      this.model.items.push(new TodoItem(value, false));
+    }
   }
 }
